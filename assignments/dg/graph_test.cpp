1bdf3d4ba5cbd1a388afba44f9f4bf8c9edb41ee
Gordon Xie <z5160618@student.unsw.edu.au>
2019-08-02 02:45:54 +1000

    added bunch of tests

--- assignments/dg/graph_test.cpp
+++ assignments/dg/graph_test.cpp
@@ -1,6 +1,22 @@
 /*
 
   == Explanation and rational of testing ==
+  The scope of our tests are mainly concerned with ensuring the correctness of each function
+  (constructor, overload, method) that we implemented. To do so we created brief tests for each
+  function (since each part of the program I implemented is relatively simple, if it works for a
+  simple case, it should work for more complicated cases too).
+
+  First, we will test all the constructors and ensure that they work. From there, we just test each method/operator on
+  any graph, since once the graph is constructed, it will behave the same with the methods/operators no matter how it was
+  initially constructed.
+
+  Then, we will test edge cases such as calling certain methods/operators on an empty graph, using non-primitive types, and iterating past
+  the end of the graph.
+
+  Furthermore, we will also test each exception to check that it is throwing the correct error and string.
+
+  Each feature should be tested with it's own individual scenario to ensure that it's clear what is being tested
+  and what could have gone wrong.
 
 DONE:
 [x] change E to a smart pointer
@@ -19,6 +35,8 @@ TODO:
 [ ] check correct errors are being thrown & return types are correct
 [ ] const correctness
 [ ] clean up some code
+[ ] edge cases
+[ ] exception throwing tests
 
 
 
@@ -39,6 +57,209 @@ bool isEqual(std::vector<T> const &v1, std::vector<T> const &v2)
             std::equal(v1.begin(), v1.end(), v2.begin()));
 }
 
+SCENARIO("Constructing an empty graph using the default constructor))") {
+    WHEN("We try to construct a graph of ints and doubles using the default constructor") {
+        gdwg::Graph<int, double> g1;
+        THEN("The graph should be successfully constructed and empty") {
+            std::vector<int> v = g1.GetNodes();
+            REQUIRE(v.empty());
+        }
+    }
+}
+
+SCENARIO("Constructing a graph using the const iterator constructor))") {
+    WHEN("We try to construct a graph of strings and doubles using the const iterator constructor") {
+        std::string p1 = "hello";
+        std::string p2 = "string";
+        std::string p3 = "cheesecake";
+        std::vector<std::string> pair_v{p1, p2, p3};
+        gdwg::Graph<std::string, double> g{pair_v.begin(), pair_v.end()};
+        THEN("The graph should be successfully constructed and contain the correct nodes in the correct order") {
+                    std::vector<std::string> v = g.GetNodes();
+                    REQUIRE(v[0] == "cheesecake");
+                    REQUIRE(v[1] == "hello");
+                    REQUIRE(v[2] == "string");
+        }
+    }
+}
+
+SCENARIO("Constructing a graph using the tuple iterator constructor))") {
+    WHEN("We try to construct a graph of strings and doubles using the tuple iterator constructor") {
+        std::string sydney{"sydney"};
+        std::string melbourne{"melbourne"};
+        std::string perth{"perth"};
+
+        auto syd_melb = std::make_tuple(sydney, melbourne, 5.4);
+        auto melb_per = std::make_tuple(melbourne, perth, 20.1);
+
+        auto e = std::vector<std::tuple<std::string, std::string, double>>{syd_melb, melb_per};
+        gdwg::Graph<std::string, double> aus{e.begin(), e.end()};
+        THEN("The graph should be successfully constructed and contain the correct nodes and edges") {
+            std::vector<std::string> v = aus.GetNodes();
+            REQUIRE(v[0] == melbourne);
+            REQUIRE(v[1] == perth);
+            REQUIRE(v[2] == sydney);
+
+            std::vector<double> syd_mel_weights = aus.GetWeights(sydney, melbourne);
+            std::vector<double> perth_mel_weights = aus.GetWeights(melbourne, perth);
+            REQUIRE(syd_mel_weights[0] == 5.4);
+            REQUIRE(perth_mel_weights[0] == 20.1);
+        }
+    }
+}
+
+SCENARIO("Constructing a graph using the initializer list constructor))") {
+    WHEN("We try to construct a graph of strings and doubles using the tuple iterator constructor") {
+        gdwg::Graph<std::string, double> graph{"red", "orange", "yellow", "green", "blue", "indigo", "violet"};
+        THEN("The graph should be successfully constructed and contain the correct nodes and edges") {
+            std::vector<std::string> colours{"blue", "green", "indigo", "orange", "red", "violet", "yellow" };
+            REQUIRE(isEqual(graph.GetNodes(), colours));
+        }
+    }
+}
+
+SCENARIO("Testing the InsertNode method") {
+    GIVEN("An empty graph of int nodes and double edges") {
+        gdwg::Graph<int, double> g;
+        WHEN("We try to insert a node into it using the InsertNode method") {
+            g.InsertNode(5);
+            THEN("We should get a graph which contains a single node with value 5") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 1);
+                REQUIRE(nodes[0] == 5);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the InsertEdge method") {
+    GIVEN("An graph of int nodes and double edges containing just two nodes") {
+        gdwg::Graph<int, double> g;
+        g.InsertNode(1);
+        g.InsertNode(2);
+        WHEN("We try to insert an edge between from one node to the other using the InsertEdge method") {
+            g.InsertEdge(1, 2, 2.5);
+            THEN("We should get a graph which contains those two nodes and an edge from the first to the second") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 2);
+                REQUIRE(nodes[0] == 1);
+                REQUIRE(nodes[1] == 2);
+
+                std::vector<double> edges = g.GetWeights(1,2);
+                REQUIRE(edges[0] == 2.5);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the DeleteNode method on an existing node") {
+    GIVEN("An graph of int nodes and double edges containing just two nodes") {
+        gdwg::Graph<int, double> g;
+        g.InsertNode(1);
+        g.InsertNode(2);
+        WHEN("We try to delete the first node using the DeleteNode method") {
+            g.DeleteNode(1);
+            THEN("We should get a graph which contains only the second node") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 1);
+                REQUIRE(nodes[0] == 2);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the DeleteNode method on a non-existent node") {
+    GIVEN("An graph of int nodes and double edges containing just two nodes") {
+        gdwg::Graph<int, double> g;
+        g.InsertNode(1);
+        g.InsertNode(2);
+        WHEN("We try to delete a non-existent node using the DeleteNode method") {
+            g.DeleteNode(3);
+            THEN("Nothing should change in the graph") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 2);
+                REQUIRE(nodes[0] == 1);
+                REQUIRE(nodes[1] == 2);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the Replace method") {
+    GIVEN("An graph of int nodes and double edges containing just two nodes") {
+        gdwg::Graph<int, double> g;
+        g.InsertNode(1);
+        g.InsertNode(2);
+        WHEN("We try to replace the first node using the Replace method") {
+            g.Replace(1, 3);
+            THEN("The value of the first node should now be set to a different one by the replace method") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 2);
+                REQUIRE(nodes[0] == 2);
+                REQUIRE(nodes[1] == 3);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the Replace method with a value that already exists") {
+    GIVEN("An graph of int nodes and double edges containing just two nodes") {
+        gdwg::Graph<int, double> g;
+        g.InsertNode(1);
+        g.InsertNode(2);
+        WHEN("We try to replace the first node with an already existing value using the Replace method") {
+            REQUIRE(!g.Replace(1, 2));
+            THEN("The value of the first node should remain the same") {
+                std::vector<int> nodes = g.GetNodes();
+                REQUIRE(nodes.size() == 2);
+                REQUIRE(nodes[0] == 1);
+                REQUIRE(nodes[1] == 2);
+            }
+        }
+    }
+}
+
+SCENARIO("Testing the MergeReplace method") {
+    GIVEN("A graph of strings and doubles") {
+        std::string sydney{"sydney"};
+        std::string melbourne{"melbourne"};
+        std::string perth{"perth"};
+        std::string adelaide{"adelaide"};
+        std::string brisbane{"brisbane"};
+
+        auto syd_melb = std::make_tuple(sydney, melbourne, 5.4);
+        auto per_syd = std::make_tuple(perth, sydney, 6.9);
+        auto melb_per = std::make_tuple(melbourne, perth, 20.1);
+        auto per_ade = std::make_tuple(perth, adelaide, 25.9);
+        auto syd_ade = std::make_tuple(sydney, adelaide, 4.7);
+        auto ade_bris = std::make_tuple(adelaide, brisbane, 2.3);
+
+        auto e = std::vector<std::tuple<std::string, std::string, double>>{syd_melb, melb_per, per_ade, syd_ade, ade_bris, per_syd};
+        gdwg::Graph<std::string, double> aus{e.begin(), e.end()};
+        WHEN("We try to replace and merge the old node with an already existing value using the MergeReplace method") {
+            aus.MergeReplace(sydney, melbourne);
+            THEN("The graph should no longer contain the old node, all outgoing edges from the old node should now be outgoing from the"
+                 "new node, and all incoming edges to the old node should now be incoming to the new node") {
+                std::vector<std::string> nodes = aus.GetNodes();
+                REQUIRE(nodes.size() == 4); // ensure that the old node has been deleted
+                REQUIRE(nodes[0] == adelaide);
+                REQUIRE(nodes[1] == brisbane);
+                REQUIRE(nodes[2] == melbourne);
+                REQUIRE(nodes[3] == perth);
+
+                std::vector<double> mel_per_weights = aus.GetWeights(melbourne, perth);
+                std::vector<double> mel_mel_weights = aus.GetWeights(melbourne, melbourne);
+                std::vector<double> mel_ade_weights = aus.GetWeights(melbourne, adelaide);
+                std::vector<double> per_mel_weights = aus.GetWeights(perth, melbourne);
+                REQUIRE(mel_per_weights[0] == 20.1);
+                REQUIRE(mel_mel_weights[0] == 5.4);
+                REQUIRE(mel_ade_weights[0] == 4.7);
+                REQUIRE(per_mel_weights[0] == 6.9);
+
+            }
+        }
+    }
+}
 
 
 SCENARIO("a default constructed graph") {
